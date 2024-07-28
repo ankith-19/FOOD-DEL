3a1c31233354aa73bc5faa3464cec8c8880fe34a
ankith-19 <162679377+ankith-19@users.noreply.github.com>
2024-07-28 18:03:08 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:3000";
+  const url = "https://food-del-backend-38c6.onrender.com";
 
   return (
     <div>
